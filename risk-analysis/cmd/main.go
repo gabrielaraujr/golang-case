a9095253b42a667bfc1d9bff8f72ead5d4850f84
Gabriel Rodrigues <gabrielaraujr@gmail.com>
2026-01-19 08:24:49 -0300

    feat: enhance main function for risk analysis service

--- risk-analysis/cmd/main.go
+++ risk-analysis/cmd/main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gabrielaraujr/golang-case/risk-analysis/internal/application/services"
+	"github.com/gabrielaraujr/golang-case/risk-analysis/internal/infrastructure/queue"
 )
 
 func main() {
-	fmt.Println("Risk Analysis Service started")
+	log.Println("[RiskAnalysis] Starting...")
+
+	// Producer
+	producer, _ := queue.NewSQSProducer(queue.SQSConfig{
+		QueueURL: os.Getenv("SQS_RISK_QUEUE_URL"),
+	})
+
+	// Service
+	analyzeService := services.NewAnalyzeProposalService(producer)
+
+	// Consumer
+	consumer, _ := queue.NewSQSConsumer(queue.SQSConsumerConfig{
+		QueueURL:    os.Getenv("SQS_PROPOSALS_QUEUE_URL"),
+		MaxMessages: 10,
+	}, analyzeService)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	_ = consumer.Start(ctx)
+	log.Println("[RiskAnalysis] Consumer started")
+
+	// Graceful shutdown
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	log.Println("[RiskAnalysis] Shutting down...")
+	_ = consumer.Stop()
 }
